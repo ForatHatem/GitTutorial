73fe4e9511227cf8900f67a2b1fc102bb7a8d821
Forat <forathatem@hotmail.com>
2021-09-29 05:05:40 -0700

    modified file_local.c

--- file_local.c
+++ file_local.c
@@ -4,5 +4,6 @@ int main (void)
 {
 	printf("Hi guys welcome to GitHub\n");
 	printf("I am debugging this on my Windows system\n");
+	printf("Thank you Windows and Code::Block, from Linux\n");
 	return 0;
 }
